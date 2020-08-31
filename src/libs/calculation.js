c07b8772c9f5c864d17e38037e513edd65f5624e
call5h5ong <call5h5ong@gmail.com>
2020-08-31 22:24:18 +0900

    처음 입력 시 0을 없앰

--- src/libs/calculation.js
+++ src/libs/calculation.js
@@ -1,4 +1,9 @@
 /*
 문자열과 숫자열을 이어 붙여줌
 */
-export const spliceNumber = (string, number) => `${string}${number}`;
+export const spliceNumber = (string, number) => {
+  if (string !== '0') {
+    return `${string}${number}`;
+  }
+  return `${number}`;
+};
